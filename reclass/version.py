71608ae3999a549d5e1efd4520cd2ad6a3b2b554
Tony Hignett <tony.hignett@thinktribe.com>
2020-04-28 13:04:39 +0100

    Fix usage of {{enum34}}.
    - Use {{enum-compat}} to not install it in python versions where it doesn't work.
    - Bump the version.

--- reclass/version.py
+++ reclass/version.py
@@ -14,7 +14,7 @@ from __future__ import unicode_literals
 RECLASS_NAME = 'reclass'
 DESCRIPTION = ('merge data by recursive descent down an ancestry hierarchy '
                '(forked extended version)')
-VERSION = '1.6.0'
+VERSION = '1.6.0.1'
 AUTHOR = 'martin f. krafft / Andrew Pickford / salt-formulas community'
 AUTHOR_EMAIL = 'salt-formulas@freelists.org'
 MAINTAINER = 'salt-formulas community'
